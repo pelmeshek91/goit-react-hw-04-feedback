ef7d6735af7fd943dbb2e2ce08b0e91b7594430f
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-04 18:59:01 +0100

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,36 @@ import { Notification } from 'components/notification/Notification';
 import { Statistics } from './statistics/Statistics';
 
 export const App = () => {
-  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const countFeedback = name => {
-    setState({
-      ...state,
-      [name]: state[name] + 1,
-    });
+    console.log(name);
+    switch (name) {
+      case 'good':
+        setGood(prevG => prevG + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevN => prevN + 1);
+        break;
+
+      case 'bad':
+        setBad(prevB => prevB + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
   const countTotalFeedback = () => {
-    const { good, neutral, bad } = state;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    let pers = Math.round((state.good / countTotalFeedback()) * 100);
+    let pers = Math.round((good / countTotalFeedback()) * 100);
     return pers ? pers : 0;
   };
 
@@ -30,14 +44,17 @@ export const App = () => {
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions onLeaveFeedback={countFeedback} options={state} />
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          onLeaveFeedback={countFeedback}
+        />
       </Section>
       <Section title="Statistics">
         {total ? (
           <Statistics
             countTotalFeedback={total}
             PositiveFeedback={pers}
-            options={state}
+            options={{ good, neutral, bad }}
           />
         ) : (
           <Notification message="There is no feedback" />
